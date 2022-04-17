07e8b2a44c1dbd5dbd55cf941489b8e5356f78cb
Mikhails S <awarebayes@gmail.com>
2022-04-17 19:27:40 +0300

    refactored

--- 6/algorithm.py
+++ 6/algorithm.py
@@ -1,14 +1,14 @@
 from typing import List
 
+from PyQt5.QtCore import QThread
+from PyQt5.QtWidgets import QApplication
+
 from my_types import Drawer, Point
 from my_types import Edge, PixelColor
 from lines import dda
 
 
-def wait_qt(delay):
-    from PyQt5.QtCore import QThread
-    from PyQt5.QtWidgets import QApplication
-
+def wait(delay):
     if delay != 0:
         QApplication.processEvents()
         QThread.msleep(delay)
@@ -19,11 +19,75 @@ def draw_edges(edges: List[Edge], drawer):
         dda(edge.p1(), edge.p2(), drawer.pixel_edge)
 
 
+def find_new_seed_pixels(x_left_edge: int, x_right_edge: int, y: int, drawer: Drawer) -> list:
+    """
+    Ищет новые затравочные пикселы на строке y
+    Начиная с x_left_edge, заканчивая x_right_edge
+    """
+    new_pixels = []
+    x = x_left_edge
+    while x <= x_right_edge:
+        bg_skipped_flag = False
+
+        # Пропускаем фон
+        while (
+                drawer.check_color(x, y) == PixelColor.BACKGROUND
+                and x < x_right_edge
+        ):
+            if not bg_skipped_flag:  # флаг, пропускали мы фон или нет
+                bg_skipped_flag = True
+            x = x + 1
+
+        if bg_skipped_flag:
+            # пришли в конец и оказались на фоне
+            if (
+                    x == x_right_edge
+                    and drawer.check_color(x, y) == PixelColor.BACKGROUND
+            ):
+                new_pixels.append([x, y])
+            # пришли куда то еще (на границу) и оказались не на фоне
+            else:
+                new_pixels.append([x - 1, y])
+
+        # Пропускаем залитую область или границы
+        x_started = x
+        while (
+                drawer.check_color(x, y) != PixelColor.BACKGROUND
+        ) and x < x_right_edge:
+            x = x + 1
+
+        if x == x_started:
+            x = x + 1
+    return new_pixels
+
+
+def find_bounds_and_fill(x, y, drawer: Drawer) -> tuple[int, int]:
+    """
+    Идет сначала направо, потом налево, заполняет, пока не доходит до границы
+    """
+    x_started = x
+    # -->
+    while drawer.check_color(x, y) != PixelColor.EDGE and x <= drawer.canvas_x_high:
+        drawer.pixel_inside(x, y)
+        x = x + 1
+
+    x_right_edge = x - 1
+
+    x = x_started
+
+    # <-
+    while drawer.check_color(x, y) != PixelColor.EDGE and x >= drawer.canvas_x_low:
+        drawer.pixel_inside(x, y)
+        x = x - 1
+
+    x_left_edge = x + 1
+    return x_left_edge, x_right_edge
+
+
 def method_with_seed(edges: List[Edge], drawer: Drawer, seed_pixel: Point, delay: int):
     draw_edges(edges, drawer)
-    stack = []
 
-    stack.append(seed_pixel)  # записываем в стек затравочный пиксель
+    stack = [seed_pixel]
 
     while stack:
         point = stack.pop()
@@ -31,97 +95,7 @@ def method_with_seed(edges: List[Edge], drawer: Drawer, seed_pixel: Point, delay
         if drawer.canvas_y_low > y or drawer.canvas_y_high < y:
             continue
 
-        wx = x  # запоминаем абсциссу
-
-        # заполнение справа
-        while drawer.check_color(x, y) != PixelColor.EDGE and x <= drawer.canvas_x_high:
-            drawer.pixel_inside(x, y)
-            x = x + 1
-
-        xr = x - 1  # запоминаем пиксель справа
-
-        x = wx
-
-        # заполнение слева
-        while drawer.check_color(x, y) != PixelColor.EDGE and x >= drawer.canvas_x_low:
-            drawer.pixel_inside(x, y)
-            x = x - 1
-
-        xl = x + 1  # запоминаем пиксель слева
-
-        x = xl
-        y = y + 1
-
-        # Ищем затравочные пиксели на строке выше
-        while x <= xr:
-            f = 0
-
-            while (
-                drawer.check_color(x, y) != PixelColor.EDGE
-                and drawer.check_color(x, y) != PixelColor.FILL
-                and x < xr
-            ):
-                if f == 0:
-                    f = 1
-                x = x + 1
-
-            if f == 1:
-                if (
-                    x == xr
-                    and drawer.check_color(x, y) != PixelColor.FILL
-                    and drawer.check_color(x, y) != PixelColor.EDGE
-                ):
-                    stack.append([x, y])
-                else:
-                    stack.append([x - 1, y])
-                f = 0
-
-            # Исследуем прерывание интервала
-            wx = x
-            while (
-                drawer.check_color(x, y) == PixelColor.EDGE
-                or drawer.check_color(x, y) == PixelColor.FILL
-            ) and x < xr:
-                x = x + 1
-
-            if x == wx:
-                x = x + 1
-
-        x = xl
-        y = y - 2
-
-        # Ищем затравочные пиксели на строке ниже
-        while x <= xr:
-            f = 0
-
-            while (
-                drawer.check_color(x, y) != PixelColor.EDGE
-                and drawer.check_color(x, y) != PixelColor.FILL
-                and x < xr
-            ):
-                if f == 0:
-                    f = 1
-                x = x + 1
-
-            if f == 1:
-                if (
-                    x == xr
-                    and drawer.check_color(x, y) != PixelColor.FILL
-                    and drawer.check_color(x, y) != PixelColor.EDGE
-                ):
-                    stack.append([x, y])
-                else:
-                    stack.append([x - 1, y])
-                f = 0
-
-            # Исследуем прерывание интервала
-            wx = x
-            while (
-                drawer.check_color(x, y) == PixelColor.EDGE
-                or drawer.check_color(x, y) == PixelColor.FILL
-            ) and x < xr:
-                x = x + 1
-
-            if x == wx:
-                x = x + 1
-        wait_qt(delay)
+        x_left_bound, x_right_bound = find_bounds_and_fill(x, y, drawer)
+        stack += find_new_seed_pixels(x_left_bound, x_right_bound, y + 1, drawer)
+        stack += find_new_seed_pixels(x_left_bound, x_right_bound, y - 1, drawer)
+        wait(delay)
